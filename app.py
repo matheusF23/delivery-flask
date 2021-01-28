4a1792b0868bde3578ac8bcbd31b7f6af8a16ea4
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-01-28 10:25:26 -0300

    Initializing app workdir

--- app.py
+++
@@ -1,8 +0,0 @@
-import views
-from flask import Flask
-
-
-def create_app():
-    app = Flask(__name__)  # Vai o nome do pacote (delivery-flask)
-    views.init_app(app)
-    return app
--- views.py
+++
@@ -1,9 +0,0 @@
-
-def init_app(app):
-    @app.route("/")
-    def index():
-        return "<h1>Hello, World</h1>"
-
-    @app.route("/contato")
-    def contato():
-        return "<h1>Página de contato</h1>"
