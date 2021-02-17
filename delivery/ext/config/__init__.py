3380704f0f00232162521347944b85aa90516eb1
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-02-17 11:57:22 -0300

    utilizacao do sqlalchemy

--- delivery/ext/config/__init__.py
+++ delivery/ext/config/__init__.py
@@ -1,6 +1,7 @@
 
 def init_app(app):
     app.config["SECRET_KEY"] = "senhasupersecreta"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///delivery.db"
 
     if app.debug:
         app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED'] = True
