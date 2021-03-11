9effea0c60aba5a968dead34db5b42343dc90575
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-03-11 12:15:58 -0300

    refactor: aplicando isort e black

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,9 @@
-
 def test_app_is_created(app):
     assert app.name == "delivery.app"
 
 
 def test_config_is_loaded(config):
-    assert config['DEBUG'] is False
+    assert config["DEBUG"] is False
 
 
 def test_request_returns_404(client):
